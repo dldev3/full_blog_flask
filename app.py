819a86976746215f3eee4941dd8a32cec4120305
d_liyanage <admliyanage@gmail.com>
2020-06-14 12:00:22 +0530

    templates added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -23,17 +23,14 @@ posts = [
     }
 ]
 
-
-
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
-    
+    return render_template('about.html', title="About Us")
 
 if (__name__ == '__main__'):
     app.run(debug=True)
