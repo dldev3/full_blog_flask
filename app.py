01c0e4c61f5c27bcef162c10b584e4c8ae868fb9
d_liyanage <admliyanage@gmail.com>
2020-06-14 20:08:00 +0530

    form-added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ce9a6ed38bc685d74b46bdb13f38e7db'
 
 posts = [
     {
@@ -31,6 +34,34 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About Us")
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if (__name__ == '__main__'):
     app.run(debug=True)
