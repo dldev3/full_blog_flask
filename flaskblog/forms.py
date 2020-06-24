fa6fdec0ef62c0e484dec7251c13ffffa58eefda
d_liyanage <admliyanage@gmail.com>
2020-06-24 12:16:13 +0530

    profile picture added

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length,Email, EqualTo, ValidationError
@@ -30,6 +31,7 @@ class LoginForm(FlaskForm):
 class UpdateProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpeg','jpg','png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from flask import render_template, url_for,flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateProfileForm
@@ -68,11 +70,24 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+
+    return picture_fn
+
+
 @app.route('/profile', methods=['POST','GET'])
 @login_required
 def profile():
     form = UpdateProfileForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
